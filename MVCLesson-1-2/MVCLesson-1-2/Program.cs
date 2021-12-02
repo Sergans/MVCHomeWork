f23ae981d622763d666f948103b3ec89870b8ce2
Сергей <gan7er@yandex.ru>
2021-12-02 18:45:55 +0300

    refact

--- MVCLesson-1-2/MVCLesson-1-2/Program.cs
+++ MVCLesson-1-2/MVCLesson-1-2/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Threading;
+using System.Collections.Generic;
 
 namespace MVCLesson_1_2
 {
+    public class Operation<T>
+    {
+        List<T> ls = new List<T>();
+    }
     class Program
     {
-        static void Main(string[] args)
+        static void Foo()
         {
             
+        }
+        static void Main(string[] args)
+        {
+            Thread f = new Thread(Foo);
+            f.Start();
+           
         }
     }
 }
