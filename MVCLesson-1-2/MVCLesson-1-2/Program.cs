0e2700bb4efa72792f4cdeac8bb9dc960120898b
Сергей <gan7er@yandex.ru>
2021-12-05 11:54:10 +0300

    refact

--- MVCLesson-1-2/MVCLesson-1-2/Program.cs
+++ MVCLesson-1-2/MVCLesson-1-2/Program.cs
@@ -6,7 +6,17 @@ namespace MVCLesson_1_2
 {
     public class Operation<T>
     {
-        List<T> ls = new List<T>();
+       public List<T> ls = new List<T>();
+        public void Add(T item)
+        {
+            Thread.Sleep(1000);
+            lock (ls) ;
+            ls.Add(item);
+        }
+        public void Delete(T item)
+        {
+            ls.Remove(item);
+        }
     }
     class Program
     {
@@ -16,9 +26,17 @@ namespace MVCLesson_1_2
         }
         static void Main(string[] args)
         {
-            Thread f = new Thread(Foo);
-            f.Start();
-           
+            Operation<string> op = new Operation<string>();
+            Thread tred = new Thread(() => op.Add("ffffff"));
+            Thread tred1 = new Thread(() => op.Add("aaaaaa"));
+            tred.Start();
+            tred1.Start();
+            Thread.Sleep(1000);
+           foreach(var s in op.ls)
+            {
+                Console.WriteLine(s);
+            }
+            
         }
     }
 }
