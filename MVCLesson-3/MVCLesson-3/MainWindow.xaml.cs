8eab8d95ed8b571e9ddb9f29a84d2da30b498cc8
Сергей <gan7er@yandex.ru>
2021-12-08 20:21:17 +0300

    ничего не понять

--- MVCLesson-3/MVCLesson-3/MainWindow.xaml.cs
+++ MVCLesson-3/MVCLesson-3/MainWindow.xaml.cs
@@ -13,16 +13,62 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace MVCLesson_3
 {
+
+    public class Comand1 : ICommand
+    {
+       
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+        public event EventHandler CanExecuteChanged;
+
+        public Comand1(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null || this.canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+    }
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+       
+
         public MainWindow()
-        {
+        {// создаем привязку команды
+            CommandBinding commandBinding = new CommandBinding();
+            // устанавливаем команду
+            commandBinding.Command = ApplicationCommands.Help;
+            // устанавливаем метод, который будет выполняться при вызове команды
+            commandBinding.Executed += Button_Click;
+            // добавляем привязку к коллекции привязок элемента Button
+            btn1.CommandBindings.Add(commandBinding);
+
+            // btn1.Command = ApplicationCommands.Copy;
             InitializeComponent();
+            var cm = new Comand1(o => { MessageBox.Show("Команда" + o.ToString()); });
+            //cm.Execute("1");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // var cm = new Comand1(o => {Text1.Text=("Команда" + o.ToString()); });
+
+            // btn1.Command = ApplicationCommands.Help;
+            MessageBox.Show("Справка по приложению");
         }
     }
 }
